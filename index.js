265bcd27f00637b3e2593d2ebf8c4384154afb59
George <georgemwazuna3@gmail.com>
2025-03-18 10:50:10 +0300

    Superbowl

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+function superbowlWin(record) {
+  let win = record.find((game) => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+// Exporting (if using modules)
+module.exports = superbowlWin;
